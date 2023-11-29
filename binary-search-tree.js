e2c16465db38413a1e8521f6309e5e9256308b1a
Jassi Bacha <klikster@gmail.com>
2023-11-29 15:59:38 -0800

    insert working, need to rethink the recursive inserting.

---
+++ binary-search-tree.js
@@ -0,0 +1,134 @@
+class Node {
+    constructor(val, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class BinarySearchTree {
+    constructor(root = null) {
+        this.root = root;
+    }
+
+    /** insert(val): insert a new node into the BST with value val.
+     * Returns the tree. Uses iteration. */
+    insert(val) {
+        // If the tree is empty, create a new node with the value and set it as the root
+        // Then return the tree to allow for method chaining
+        if (!this.root) {
+            this.root = new Node(val);
+            return this;
+        }
+
+        // Start from the root of the tree
+        let currentNode = this.root;
+
+        // Use a while loop to find the correct position for the new value
+        while (true) {
+            // Check if the value should be inserted in the left subtree
+            if (val < currentNode.val) {
+                // If the left child does not exist, insert the new node here
+                if (!currentNode.left) {
+                    currentNode.left = new Node(val);
+                    // Return the tree to allow for method chaining
+                    return this;
+                } else {
+                    // If the left child exists, move to the left child and continue the search
+                    currentNode = currentNode.left;
+                }
+            }
+            // Check if the value should be inserted in the right subtree
+            else if (val > currentNode.val) {
+                // If the right child does not exist, insert the new node here
+                if (!currentNode.right) {
+                    currentNode.right = new Node(val);
+                    // Return the tree to allow for method chaining
+                    return this;
+                } else {
+                    // If the right child exists, move to the right child and continue the search
+                    currentNode = currentNode.right;
+                }
+            }
+            // If the value is equal to the current node's value, don't insert it (disallow duplicates)
+            else {
+                return this;
+            }
+        }
+    }
+
+    /** insertRecursively(val): insert a new node into the BST with value val.
+     * Returns the tree. Uses recursion. */
+
+    insertRecursively(val) {
+        // Create a new node
+        const newNode = new Node(val);
+
+        // If the tree is empty, set the root to the new node
+        if (!this.root) {
+            this.root = newNode;
+            return;
+        }
+
+        // If the value inserted is greater than the root, go right
+        if (val > this.root.val) {
+            // If there is no right child, set the right child to the new node
+            if (!this.root.right) {
+                this.root.right = newNode;
+            } else {
+                // Recursively insert the value into the right subtree
+                this.insert(val, this.root.right);
+            }
+        }
+        // If the value inserted is less than the root, go left
+    }
+
+    /** find(val): search the tree for a node with value val.
+     * return the node, if found; else undefined. Uses iteration. */
+
+    find(val) {}
+
+    /** findRecursively(val): search the tree for a node with value val.
+     * return the node, if found; else undefined. Uses recursion. */
+
+    findRecursively(val) {}
+
+    /** dfsPreOrder(): Traverse the array using pre-order DFS.
+     * Return an array of visited nodes. */
+
+    dfsPreOrder() {}
+
+    /** dfsInOrder(): Traverse the array using in-order DFS.
+     * Return an array of visited nodes. */
+
+    dfsInOrder() {}
+
+    /** dfsPostOrder(): Traverse the array using post-order DFS.
+     * Return an array of visited nodes. */
+
+    dfsPostOrder() {}
+
+    /** bfs(): Traverse the array using BFS.
+     * Return an array of visited nodes. */
+
+    bfs() {}
+
+    /** Further Study!
+     * remove(val): Removes a node in the BST with the value val.
+     * Returns the removed node. */
+
+    remove(val) {}
+
+    /** Further Study!
+     * isBalanced(): Returns true if the BST is balanced, false otherwise. */
+
+    isBalanced() {}
+
+    /** Further Study!
+     * findSecondHighest(): Find the second highest value in the BST, if it exists.
+     * Otherwise return undefined. */
+
+    findSecondHighest() {}
+}
+
+module.exports = BinarySearchTree;
